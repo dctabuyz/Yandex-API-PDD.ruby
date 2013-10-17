b1085e5effac4c4afa0a68accfae54bb1f20d417
dctabuyz <dctabuyz@yandex.ru>
2013-10-18 02:13:14 +0400

    fix install

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,40 +1,31 @@
 require "rubygems"
 require "rubygems/command_manager"
 
-ok  = 0
-
-cmd = Gem::CommandManager::new
-
 begin
 	require "libxml"
-	ok = 1
 
 rescue LoadError
 
-	print "libxml not found, trying to install\n"
-
-	$stdout.flush
+	libxml_install_exception = nil
 
 	begin
-		ok = cmd.run("install libxml-ruby")
+		Gem::CommandManager.instance.run("install libxml-ruby")
 
-	ensure 
-		begin
-			require "libxml"
-			ok = 1
+	rescue Exception => libxml_install_exception
+	# XXX intended to be empty
+	end
 
-		rescue LoadError
+	begin
+		require "libxml"
 
-			ok = 0
-		end
+	rescue LoadError
+
+		raise libxml_install_exception unless libxml_install_exception.nil?
 	end
 end
 
-raise Gem::InstallError, "Failed dependencies to install: libxml" unless ok
+filename = File.expand_path("..", __FILE__) + "/Makefile"
 
-n = File.expand_path("..", __FILE__) + "/Makefile"
-
-f = File::open(n, "w")
-f.write("install:\n")
-f.write("\t@echo fake libxml install\n")
-f.close
+makefile = File::open(filename, "w")
+makefile.write("install:\n\t@echo fake libxml install\n")
+makefile.close
